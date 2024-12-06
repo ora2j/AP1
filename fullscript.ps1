31d5d87d18f296b3b82ab4f3e60e13efee4ea501
ora2j <68998445+ora2j@users.noreply.github.com>
2024-12-06 09:02:26 +0100

    Add files via upload

---
+++ fullscript.ps1
@@ -0,0 +1,18 @@
+#General script for windows 10 hardening
+
+#Defender
+    Update-MpSignature
+    Set-MpPreference -DisableRealtimeMonitoring 0
+    Set-MpPreference -SignatureScheduleDay Everyday
+    Set-MpPreference -CheckForSignaturesBeforeRunningScan 1
+    Set-MpPreference -ScanPurgeItemsAfterDelay 30
+    Set-MpPreference -AllowNetworkProtectionOnWinServer 1
+    Set-MpPreference -AllowSwitchToAsyncInspection 1
+    Set-MpPreference -CloudBlockLevel 2
+    Set-MpPreference -DisableArchiveScanning 0
+    Set-MpPreference -DisableAutoExclusions 1
+    Set-MpPreference -DisableDatagramProcessing 0
+    Set-MpPreference -DisableDnsOverTcpParsing 0
+    Set-MpPreference -DisableDnsParsing 
+    Set-MpPreference -DisableIOAVProtection $false
+    Set-MpPreference -DisableNetworkProtectionPerfTelemetry $false
---
+++ script.sh
@@ -0,0 +1,14 @@
+#DPKG 
+
+    sudo apt update
+    sudo apt install unattended-upgrades
+    sudo ufw default deny incoming
+    sudo apt install okular
+    sudo apt install fail2ban
+    sudo apt install clamav
+    sudo apt install lynis
+
+#config
+
+
+    
